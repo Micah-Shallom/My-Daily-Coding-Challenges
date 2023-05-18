9be9d348c7cb51a35e230375c61aeb799b365cff
Micah <micahshallom@gmail.com>
2023-05-18 11:02:01 +0100

    greatest common divisor

--- dataStructures/Recursions/power.py
+++ dataStructures/Recursions/power.py
@@ -1,7 +1,8 @@
 def power(n,exp):
+    assert exp>=0 and int(exp) == exp , "Only positive integers are required"
     if  exp in [0,1]:
         return n
     else:
-        return n*(power(n,exp-1))
+        return n*(power(n,exp-1)) 
 
-print(power(5,3))
+print(power(-3.4,3))
