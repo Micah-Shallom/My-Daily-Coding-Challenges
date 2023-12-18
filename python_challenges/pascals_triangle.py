5db29e5b09e9bcfd0e7f82f7a9cfda9db15d162f
Micah <micahshallom@gmail.com>
2023-12-18 10:10:06 +0100

    python solution unqiue emails

--- python_challenges/pascals_triangle.py
+++ python_challenges/pascals_triangle.py
@@ -15,8 +15,8 @@
 # 1 <= numRows <= 30
 class Solution:
     def generate(self, numRows: int) -> list[list[int]]:
-        triangle = [[1], [1, 1]]
-        if numRows == 1:
+        triangle = [[1], [1, 1]]`
+        if numRows == 1:`
             return triangle[0]
         for i in range(2, numRows):
             prevPascal = triangle[i - 1]
@@ -25,11 +25,27 @@ class Solution:
                 triangle[i].append(prevPascal[j] + prevPascal[j + 1])
             triangle[i].append(1)
         return triangle
+    
+    def generate1(self, numRows: int) -> list[list[int]]:
+        triangle = []
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            triangle.append(row)
+        return triangle
+
+    
+    def recursiveGenerate(self, numRows: int) -> list[list[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        prevRows = self.recursiveGenerate(numRows-1)
+        newRow = [1] * numRows
+        print(newRow)
+        return prevRows
 
 solution = Solution()
 numRows = 5
-print(solution.generate(numRows))
-
-solution = Solution()
-numRows = 5
-print(solution.generate(numRows))
+print(solution.generate1(numRows))
