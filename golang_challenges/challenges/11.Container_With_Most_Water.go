09f503bc5bd1d3c305e5787639bc7d29c24e491a
Micah <micahshallom@gmail.com>
2024-01-13 11:00:00 +0100

    11.container with most water

--- golang_challenges/challenges/11.Container_With_Most_Water.go
+++ golang_challenges/challenges/11.Container_With_Most_Water.go
@@ -13,7 +13,7 @@
 
 // Input: height = [1,8,6,2,5,4,8,3,7]
 // Output: 49
-// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+// Explanation: The above vertical line are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 // Example 2:
 
 // Input: height = [1,1]
