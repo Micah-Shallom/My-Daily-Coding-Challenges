e9b39d7146ffd3ad59ca415053e5967a1793a446
Micah <micahshallom@gmail.com>
2024-02-19 12:00:00 +0100

    409.longest palindrome

---
+++ golang_challenges/challenges/409.Longest_Palindrome.go
@@ -0,0 +1,26 @@
+//
+
+package challenges
+
+func longestPalindrome(s string) int {
+	charCount := make(map[rune]int)
+
+	for _, char := range s {
+		charCount[char]++
+	}
+
+	palindromeLength := 0
+	odd := false
+	for _, count := range charCount {
+		if count%2 == 0 {
+			palindromeLength += count
+		} else {
+			palindromeLength += count - 1
+			odd = true
+		}
+	}
+	if odd {
+		palindromeLength++
+	}
+	return palindromeLength
+}
