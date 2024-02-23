f6fc259ce534dc390bc81c8b7c23ff5908b13015
Micah <micahshallom@gmail.com>
2024-02-23 06:54:56 +0100

    selection sort

--- python-algorithms/sorting/selectionSort.py
+++ python-algorithms/sorting/selectionSort.py
@@ -1,6 +1,11 @@
 def selectionSort(customList):
-    pass
-
+    for i in range(len(customList)):
+        min_index = i
+        for j in range(i + 1, len(customList)):
+            if customList[min_index] > customList[j]:
+                min_index = j
+        customList[i], customList[min_index] = customList[min_index], customList[i]
+    return customList
 
 cList = [3,6,8,12,4,9,1,0,6]
 print(selectionSort(cList))
