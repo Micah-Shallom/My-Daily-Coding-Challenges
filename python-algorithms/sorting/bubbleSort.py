69009fe4c879582d8d0a93f94fa5bead4a92ceab
Micah <micahshallom@gmail.com>
2024-02-23 01:56:12 +0100

    bubble sort

---
+++ python-algorithms/sorting/bubbleSort.py
@@ -0,0 +1,10 @@
+def bubblesort(customList):
+    for i in range(len(customList) - 1):
+        for j in range(len(customList) - i - 1):
+            if customList[j] > customList[j+1]:
+                customList[j], customList[j+1] = customList[j+1], customList[j]
+    return customList
+
+
+cList = [3,6,8,12,4,9,1,0,6]
+print(bubblesort(cList))
---
+++ python-algorithms/sorting/selectionSort.py
@@ -0,0 +1,6 @@
+def selectionSort(customList):
+    pass
+
+
+cList = [3,6,8,12,4,9,1,0,6]
+print(selectionSort(cList))
