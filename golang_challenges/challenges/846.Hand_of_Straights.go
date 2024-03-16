83b146ca07248ede109fac2d3a846c3417f862aa
Micah <micahshallom@gmail.com>
2024-03-16 12:00:00 +0100

    846.hand of straights

--- golang_challenges/challenges/846.Hand_of_Straights.go
+++ golang_challenges/challenges/846.Hand_of_Straights.go
@@ -1,7 +1,6 @@
 // Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
 
-// Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise. If Alice can rearrange the cards, she can choose any group to start with.
-
+// Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
  
 
 // Example 1:
