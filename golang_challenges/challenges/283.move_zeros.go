4c2fac9fe4b30320c4cb2945f66818a05776ede1
Micah <micahshallom@gmail.com>
2024-04-26 08:36:40 +0100

    283.move_zeros

---
+++ golang_challenges/challenges/283.move_zeros.go
@@ -0,0 +1,26 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+package challenges
+
+func moveZeroes(nums []int)  {
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+		}
+	}
+}
